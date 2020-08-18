4f679ed3a40082b52cd47f7db5e74750ed6a5b41
brennomatheus <brenno_silva_@hotmail.com>
2020-08-18 17:37:28 -0300

    refactor: rename update with updateAccessToken

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,11 +1,11 @@
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { makeSignUpValidation } from './signup-validation'
 import { LogControllerDecorator } from '../../decorators/log'
 import { Controller } from '../../../presentation/protocols'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
