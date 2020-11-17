0d699fcf28065829bb5bda0f23290de28d2769da
brennomatheus <brenno_silva_@hotmail.com>
2020-11-16 23:45:34 -0300

    test: ensure AddSurveyController returns 500 if AddSurveyUseCase throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,7 +1,7 @@
 import { HttpRequest,AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
 import { Validation } from '../../../protocols'
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../../helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -62,9 +62,8 @@ describe('', () => {
   test('Should return 400 if validation fails', async () => {
     const { sut,validationStub } = makeSut()
     jest.spyOn(validationStub,'validate').mockReturnValueOnce(new Error())
-    const httpRequest = makeFakeRequest()
 
-    const httpResponse = await sut.handle(httpRequest)
+    const httpResponse = await sut.handle(makeFakeRequest())
 
     expect(httpResponse).toEqual(badRequest(new Error()))
   })
@@ -73,9 +72,17 @@ describe('', () => {
     const { sut,addSurveyStub } = makeSut()
     const addSpy = jest.spyOn(addSurveyStub,'add')
     const httpRequest = makeFakeRequest()
-
     await sut.handle(httpRequest)
 
     expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 500 if AddSurvey thows', async () => {
+    const { sut,addSurveyStub } = makeSut()
+    jest.spyOn(addSurveyStub,'add').mockReturnValueOnce(new Promise((resolve,reject) => reject(Error())))
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
