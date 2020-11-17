0d8c06ecef029154e5b6988a4dd0d85a42f89713
brennomatheus <brenno_silva_@hotmail.com>
2020-11-16 23:11:54 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -8,18 +8,38 @@ const makeFakeRequest = (): HttpRequest => ({
     answers: [{ image: 'any_image',answer: 'any_answer' }]
   }
 })
+
+interface SutTypes {
+  sut: AddSurveyController
+  validationStub: Validation
+}
+
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+
+  return new ValidationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new AddSurveyController(validationStub)
+
+  return {
+    sut,
+    validationStub
+  }
+}
+
 describe('', () => {
   test('Should call validation with correct values', async () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return null
-      }
-    }
-
-    const validationStub = new ValidationStub()
+    const { sut,validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub,'validate')
-    const sut = new AddSurveyController(validationStub)
     const httpRequest = makeFakeRequest()
+
     await sut.handle(httpRequest)
 
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
